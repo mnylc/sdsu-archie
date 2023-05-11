1fae7d484f83d70429e18b10a41d6be1ccacf4c7
Diego Pino Navarro <dpino@metro.org>
2023-05-10 21:06:18 -0400

    Adds Glide.js

--- js/global.js
+++ js/global.js
@@ -10,6 +10,7 @@
     Drupal.behaviors.archipelago_subtheme = {
         attach: function (context, settings) {
             $(context).find('body').once('archipelago-subtheme-global').each(function () {
+
                 window.addEventListener('resize', function(event) {
                     // de-collapses if collapsed and media query restores large viewport.
                     let MOBILE_WIDTH = 992;
@@ -20,6 +21,7 @@
 
                 // Triggers admin toolbar offset on page load
                 if (context === document) {
+                    new Glide('.glide').mount();
                     $(context).on("drupalViewportOffsetChange.toolbar", function (event, offsets) {
                         let $body = $("body");
                         if ($body.length > 0 && offsets.top > 0 && $body.css('padding-top') !== offsets.top) {
@@ -31,4 +33,4 @@
         );
     }
 }
-})(jQuery, Drupal);
+})(jQuery, Drupal, Glide);
